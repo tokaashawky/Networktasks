45b1fcab3108a73a917a8b18b56f5955b3436a30
tokaashawky <119989493+tokaashawky@users.noreply.github.com>
2024-05-13 19:18:33 +0300

    Update sever.py

--- simplechat/sever.py
+++ simplechat/sever.py
@@ -1,28 +1,32 @@
 from socket import *
 s = socket(AF_INET,SOCK_STREAM)
-print("socket successfully created")
+print("socket created")
 host='127.0.0.1'
-port= 8000
+port= 4000
 s.bind((host,port))
-print("socket bind at: ",port)
+print("socket binded to ",port)
 s.listen(5)
-print("socket is listening ^_^")
+print("socket is listening")
 c,addr = s.accept()
-print('get connection from: ', addr)
+print('get connection from', addr)
 
 while True:
     x=c.recv(2048)
     print("client:",x.decode('utf-8'))
-    if x.decode('utf-8') == "end" :
+
+    if x.decode('utf-8') == "bye" :
         break
+        
     msg= input("server: ").encode('utf-8')
-    msg_length = len(msg)
-    if msg_length > 2048:
-        chunks = msg_length // 2048 + 1
-        for i in range(chunks):
-            start = i * 2048
-            end = min((i + 1) * 2048, msg_length)
-            s.send(msg[start:end])
+
+    l = len(msg)
+    if l > 2048:
+        x = l // 2048 + 1
+        for i in range(x):
+            begin = i * 2048
+            end = min((i + 1) * 2048, l)
+            s.send(msg[begin:end])
     else:
         c.send(msg)
+       
 c.close()
